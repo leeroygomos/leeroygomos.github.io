50ca866948c30265c48076b0852299c5a7c7e900
Lee Roy Gomos <leeroy.gomos@gmail.com>
2023-05-22 01:18:44 -0700

    added icons

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,6 @@
 import './Contact.scss';
+import IconGithub from './icons/IconGithub';
+import IconLinkedin from './icons/IconLinkedin';
 
 export default function Contact(){
     return (
@@ -7,19 +9,40 @@ export default function Contact(){
                 <p>
                     I am currently open to new opportunities. Feel free to connect with me on LinkedIn or email me at leeroy.gomos@gmail.com
                 </p>
+                <div className='icons'>
+                    <a href='https://github.com/leeroygomos' target='_blank' rel='noreferrer'>
+                        <IconGithub />
+                    </a>
+                    <a href='https://www.linkedin.com/in/lee-roy-gomos-6077b1125/' target='_blank' rel='noreferrer'>
+                        <IconLinkedin />
+                    </a>
+                </div>
             </div>
 
             <div className='right-container'>
                 <h1>Contact Me</h1>
                 <form className='email-form'>
-                    <label htmlFor='subject'>Subject</label>
-                    <input type='text' name='subject' id='subject' />
-                    <label htmlFor='name'>Name</label>
-                    <input type='text' name='name' id='name' />
-                    <label htmlFor='fromEmail'>Email</label>
-                    <input type='text' name='fromEmail' id='fromEmail' />
-                    <label htmlFor='body'>Body</label>
-                    <textarea name='body' id='body'>body</textarea>
+                    <div className='top-row'>
+                        <div className='name'>
+                            <label htmlFor='name'>Name</label>
+                            <input type='text' name='name' id='name' />
+                        </div>
+                        <div className='email'>
+                            <label htmlFor='fromEmail'>Email</label>
+                            <input type='text' name='fromEmail' id='fromEmail' />
+                        </div>
+                    </div>
+                    <div className='subject'>
+                        <label htmlFor='subject'>Subject</label>
+                        <input type='text' name='subject' id='subject' />
+                    </div>
+                    <div className='body'>
+                        <label htmlFor='body'>Body</label>
+                        <textarea name='body' id='body'>body</textarea>
+                    </div>
+                    <div className='submit'>
+                        <input type='submit' value='Submit' />
+                    </div>
                 </form>
             </div>
         </div>
